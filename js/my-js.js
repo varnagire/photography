28014270a0bd3ab18914ec0efd9c08b7bfa62975
Migle <migle.varnagire@gmail.com>
2015-06-28 20:28:23 +0100

    Trying to make masonry disabled

--- js/my-js.js
+++ js/my-js.js
@@ -1,15 +1,16 @@
-
+/*global $ */
 //// Masonry grid  
 //
-$(document).ready(function(){
-  var $container = $('.container');
-
-  $container.imagesLoaded( function() {
-    $container.masonry({
-      itemSelector        : '.photo',
-      columnWidth         : '.photo',
-      transitionDuration  : 0
-    });
-  });
+$(document).ready(function () {
+    var $container = $('.container');
+    
+    if ($(window).width() >= 767) {
+        $container.imagesLoaded(function () {
+            $container.masonry({
+                itemSelector        : '.photo',
+                columnWidth         : '.photo',
+                transitionDuration  : 0
+            });
+        });
+    }
 });
-
